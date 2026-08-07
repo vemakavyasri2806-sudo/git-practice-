63af169792e11317e9de818a41c77339e4fbb73f
Kavya Sri <vemakavyasri2806@gmail.com>
2026-08-07 17:15:09 +0530

    student details

---
+++ student.py
@@ -0,0 +1,11 @@
+student_name = "kavya sri"
+course = "BCA"
+college = "Ideal Degree College For Women"
+age = "20"
+
+print("student details")
+print("-----------------")
+print("name:" ,student_name)
+print("course:" ,course)
+print("college name:", college)
+print("Age:",age)
