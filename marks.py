fd4e11b9ea0cece585419801a2d86fe68569928c
Kavya Sri <vemakavyasri2806@gmail.com>
2026-08-07 17:23:25 +0530

    Added marks calculator

---
+++ marks.py
@@ -0,0 +1,15 @@
+python = 90
+dbms = 80
+java = 75
+os = 82
+networking = 65
+total = python+dbms+java+os+networking
+average = total/5
+
+print("total marks:",total)
+print("Average:",average)
+if average>=35:
+    print("result: pass")
+else:
+    print("result:Fail")
+    
